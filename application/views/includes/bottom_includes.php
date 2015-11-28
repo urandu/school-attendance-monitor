123d58281919c18b312743b2b7793e2dc0b9f7fa
urandu <bildadnamawa@gmail.com>
2015-11-28 19:44:39 +0300

    time to hit the road!!!

--- application/views/includes/bottom_includes.php
+++ application/views/includes/bottom_includes.php
@@ -1,7 +1,19 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: urandu
- * Date: 11/28/15
- * Time: 6:58 PM
- */ 
+
+</div>
+<!-- /#wrapper -->
+
+<!-- jQuery -->
+<script src="<?php echo(base_url()); ?>ui/bower_components/jquery/dist/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="<?php echo(base_url()); ?>ui/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="<?php echo(base_url()); ?>ui/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="<?php echo(base_url()); ?>ui/dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
